89a929fa781dc9599be1c6b41edf2a3711db8414
Pavel Ratushnyi <pratushnyi@smartling.net>
2015-02-04 02:27:45 +0200

    fix/refactor according to https://cr.smartling.net/cru/CR-PHP-CMS-MODULES-48

--- inc/Smartling/Exception/SmartlingException.php
+++ inc/Smartling/Exception/SmartlingException.php
@@ -2,8 +2,6 @@
 
 namespace Smartling\Exception;
 
-use Smartling\Bootstrap;
-
 /**
  * Class SmartlingException
  *
@@ -13,35 +11,4 @@ use Smartling\Bootstrap;
  */
 abstract class SmartlingException extends \Exception {
 
-	/**
-	 * @param string     $message
-	 * @param int        $code
-	 * @param \Exception $previous
-	 */
-	public function __construct ( $message = '', $code = 0, \Exception $previous = null ) {
-		parent::__construct( $this, $code, $previous );
-
-		$this->tryLogException( $this );
-	}
-
-
-	/**
-	 * @param SmartlingException $exception
-	 *
-	 * @throws \Exception
-	 */
-	private function tryLogException ( SmartlingException $exception ) {
-		try {
-			$message = $exception->getMessage();
-
-			$fileString = vsprintf( '%s : %d', array ( $exception->getFile(), $exception->getLine() ) );
-
-			$trace = $exception->getTraceAsString();
-
-			Bootstrap::getLogger()->error( $message, array ( $fileString, $trace ) );
-
-		} catch ( \Exception $e ) {
-			throw $e;
-		}
-	}
 }
