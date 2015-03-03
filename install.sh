17a58911f92c7ab606fcea70eeb6d28b8d0f9d20
Pavel Ratushnyi <pratushnyi@smartling.net>
2015-03-03 15:53:45 +0200

    install script updated: + added echo messages about script activity * fixed bug that only first dependency is installed

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 BASEDIR=$(dirname $0)
 
+RUNDATE=$(date +"wp-autoinstall-%Y%m%d%H%M%S")
+
 logit () {
     DATE=$(date +"[%Y-%m-%d %H:%M:%S]")
     echo -n "$DATE "
@@ -21,14 +23,14 @@ logit () {
 usage () {
     echo -e "Usage:\n\t$0 --site-url=<url> --database-host=<host> --database-user=<user> --database-pass=<pass> --database-name=<name> --site-title=<title> --site-user=<login> --site-pass=<password>"
     echo -e "\n\nWhere:"
-    echo -e "\t<url>\t\t- The final of Wordpress URL. Note it is quite hard to change it after installation! E.g.: mysite.com"
-    echo -e "\t<host>\t\t- The hostname/ip of MySQL server"
-    echo -e "\t<user>\t\t- The username of MySQL server for Wordpress installation. Is used to create database"
-    echo -e "\t<pass>\t\t- The password for database user"
-    echo -e "\t<name>\t\t- The Wordpress database name. Note: Script will try to delete it and create."
-    echo -e "\t<title>\t\t- The title of the installed Wordpress main blog. Due to installer limitations should be ONE WORD."
-    echo -e "\t<login>\t\t- The wordpress admin login."
-    echo -e "\t<password>\t- The wordpress admin password."
+    echo -e "\t--site-url\t- The Wordpress URL. Note it is quite hard to change it after installation! E.g.: mysite.com or localhost"
+    echo -e "\t--database-host\t- The hostname/ip of MySQL server. Optional. Default:localhost"
+    echo -e "\t--database-user\t- The username of MySQL server for Wordpress installation. Is used to create database"
+    echo -e "\t--database-pass\t- The password for database user"
+    echo -e "\t--database-name\t- The Wordpress database name. Note: Script will try to delete it and create."
+    echo -e "\t--site-title\t- The title of the installed Wordpress main blog. Due to installer limitations should be ONE WORD. Optional. Default:Demosite"
+    echo -e "\t--site-user\t- The wordpress admin login. Optional. Default:wp"
+    echo -e "\t--site-pass\t- The wordpress admin password. Optional. Default:wp"
     exit 1
 }
 
@@ -53,46 +55,40 @@ while [ $# -gt 0 ];do
     shift
 done
 
-
-
-
-
 [ "x$SITE_URL" == "x" ] && { logit err "--site-url should be set"; usage; }
-[ "x$MYSQL_HOST" == "x" ] && { logit err "--database-host should be set"; usage; }
+[ "x$MYSQL_HOST" == "x" ] && { logit warn "--database-host is not set. Using 'localhost'"; MYSQL_HOST=localhost; }
 [ "x$MYSQL_USER" == "x" ] && { logit err "--database-user should be set"; usage; }
 [ "x$MYSQL_PASS" == "x" ] && { logit err "--database-pass should be set"; usage; }
-[ "x$MYSQL_BASE" == "x" ] && { logit err "--database-name should be set"; usage; }
-[ "x$SITE_TITLE" == "x" ] && { logit err "--site-title should be set"; usage; }
-[ "x$SITE_USER" == "x" ] && { logit err "--site-user should be set"; usage; }
-[ "x$SITE_PASS" == "x" ] && { logit err "--site-pass should be set"; usage; }
+[ "x$MYSQL_BASE" == "x" ] && { logit warn "--database-name is not set. Using '$RUNDATE'"; MYSQL_BASE=$RUNDATE; }
+[ "x$SITE_TITLE" == "x" ] && { logit warn "--site-title is not set. Using 'Demosite'"; SITE_TITLE=Demosite; }
+[ "x$SITE_USER" == "x" ] && { logit warn "--site-user is not set. Using 'wp'"; SITE_USER=wp; }
+[ "x$SITE_PASS" == "x" ] && { logit warn "--site-pass is not set. Using 'wp'"; SITE_PASS=wp; }
 
 ## dependencies for smartling-connector, separated by ';'
-DEPENDENCIES=multilingual-press;ad-code-manager;post-customizer;wordpress-seo;facebook
-
+DEPENDENCIES="multilingual-press;ad-code-manager;post-customizer;wordpress-seo;facebook"
+echo "Downloading wp.cli"
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 WPCLI=./wp-cli.phar
-
+echo "Downloading latest Wordpress distribution"
 # Downloading last version of wordpress
 $WPCLI core download
-
+echo "Generating wp-config.php file"
 # Generating wp-config.php file
 $WPCLI core config --dbname=$MYSQL_BASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASS
-
+echo "Trying to drop and create database"
 SQL_RESET_DB="DROP DATABASE IF EXISTS \`$MYSQL_BASE\`;CREATE DATABASE IF NOT EXISTS \`$MYSQL_BASE\` DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;"
-
 echo $SQL_RESET_DB > ./db-reset.sql
-
 mysql -u$MYSQL_USER -p$MYSQL_PASS < ./db-reset.sql
-
 rm ./db-reset.sql
-
+echo "Setting up multisite Wordpress installation"
 # newtwork installing wordpress
 $WPCLI core multisite-install --url=$SITE_URL --base=/ --title=$SITE_TITLE --admin_user=$SITE_USER --admin_password=$SITE_PASS --admin_email=no@e.mail
-
-dep_arr=$(echo $DEPENDENCIES | tr ";" "\n")
-
-for dep in $dep_arr
+echo "Installing plugins..."
+IFS=';' read -a array <<< "$DEPENDENCIES"
+for plugin_name in "${array[@]}"
 do
-$WPCLI plugin install $dep
-$WPCLI plugin activate $dep
+echo "Installing plugin $plugin_name"
+$WPCLI plugin install $plugin_name
+echo "Activating plugin $plugin_name"
+$WPCLI plugin activate $plugin_name
 done
