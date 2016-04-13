e49bfb32082ead3be8cea33799c0f2727a5cce0d
Pavlo Ratushnyi <pratushnyi@smartling.com>
2016-04-13 15:25:27 +0300

    remove obsoleted tests

--- tests_old/FileUriHelperTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class FileUriHelperTest extends PHPUnit_Framework_TestCase
-{
-
-   public function testFileUriGeneration(){}
-
-}
--- tests_old/phpunit_bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once realpath( __DIR__ ) . './../inc/autoload.php';
-
-require_once '_wordpress_functions_mock.php';
