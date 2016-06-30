a5a2782aba144a6cbf6bfed01ed5aa1f95b95e5a
Pavlo Ratushnyi <pratushnyi@smartling.com>
2016-06-30 11:36:27 +0300

    WP-221 Exclude shortcodes from translation by smartling custom placeholder regex

--- inc/Smartling/Helpers/XmlEncoder.php
+++ inc/Smartling/Helpers/XmlEncoder.php
@@ -35,7 +35,8 @@ class XmlEncoder
     private static $magicComments = ['smartling.translate_paths = data/string',
                                      'smartling.string_format_paths = html : data/string',
                                      'smartling.source_key_paths = data/{string.key}',
-                                     'smartling.variants_enabled = true',];
+                                     'smartling.variants_enabled = true',
+                                     'smartling.placeholder_format_custom = \[[^\]]+?\]',];
 
     const XML_ROOT_NODE_NAME = 'data';
 
