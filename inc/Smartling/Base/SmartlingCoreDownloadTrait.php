f1a273709d7a264f55f4ead7630314455ffd26c4
Pavlo Ratushnyi <pratushnyi@smartling.com>
2017-10-23 19:07:51 +0300

    WP-313 Fixed issue when download job stops on any error

--- inc/Smartling/Base/SmartlingCoreDownloadTrait.php
+++ inc/Smartling/Base/SmartlingCoreDownloadTrait.php
@@ -19,13 +19,6 @@ trait SmartlingCoreDownloadTrait
                 $entity->getTargetId(),
             ]);
             $this->getLogger()->warning($msg);
-
-            return [
-                vsprintf(
-                    'Translation of file %s for %s locale is locked for downloading',
-                    [$entity->getFileUri(), $entity->getTargetLocale()]
-                ),
-            ];
         }
         if (1 === $entity->getIsCloned()) {
             $msg = vsprintf('Triggered download of cloned entity. Target Blog: %s; Target Id: %s', [
@@ -33,24 +26,22 @@ trait SmartlingCoreDownloadTrait
                 $entity->getTargetId(),
             ]);
             $this->getLogger()->warning($msg);
-
-            return ['There is no translation since entity is Cloned, not Translated'];
         }
         if (0 === $entity->getTargetId()) {
             //Fix for trying to download before send.
             do_action(ExportedAPI::ACTION_SMARTLING_SEND_FILE_FOR_TRANSLATION, $entity);
         }
-        $messages = [];
 
-        $data = (string) $this->getApiWrapper()->downloadFile($entity);
-        $this->getLogger()
-            ->debug(vsprintf('Downloaded file for submission id = \'%s\'. Dump: %s',
-                             [$entity->getId(), base64_encode($data)]));
+        try {
+            $data = (string)$this->getApiWrapper()->downloadFile($entity);
+            $msg = vsprintf('Downloaded file for submission id = \'%s\'. Dump: %s', [$entity->getId(), base64_encode($data)]);
+            $this->getLogger()->debug($msg);
+            $this->applyXML($entity, $data);
+        } catch (\Exception $e) {
+            $msg = vsprintf('Error occurred while downloading translation for submission id=\'%s\'. Message: %s.', [$entity->getId(), $e->getMessage()]);
+            $this->getLogger()->error($msg);
+        }
 
-        $this->applyXML($entity, $data);
-
-
-        return $messages;
     }
 
     public function downloadTranslationBySubmissionId($id)
