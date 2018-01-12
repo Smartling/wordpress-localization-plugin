86de69017b89fec74c6b12acf0755b429e1d1f95
Loparev <pavel.loparev.brdm2@gmail.com>
2018-01-12 13:09:36 +0200

    Logger in EntityHelper class

--- inc/Smartling/Helpers/EntityHelper.php
+++ inc/Smartling/Helpers/EntityHelper.php
@@ -5,6 +5,7 @@ namespace Smartling\Helpers;
 use Psr\Log\LoggerInterface;
 use Smartling\DbAl\LocalizationPluginProxyInterface;
 use Smartling\Exception\SmartlingDbException;
+use Smartling\MonologWrapper\MonologWrapper;
 use Smartling\Settings\SettingsManager;
 
 /**
@@ -34,6 +35,13 @@ class EntityHelper
      * @var LoggerInterface
      */
     private $logger;
+
+    /**
+     * EntityHelper constructor.
+     */
+    public function __construct() {
+        $this->setLogger(MonologWrapper::getLogger(get_called_class()));
+    }
 
     /**
      * @return PluginInfo
