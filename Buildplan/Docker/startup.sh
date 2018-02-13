51182fa5adcbc359cd79a17d41d56fefe1c52d97
Loparev <pavel.loparev.brdm2@gmail.com>
2018-02-13 12:47:42 +0200

    Used -w instead of forwarding workspace inside of a container

--- Buildplan/Docker/startup.sh
+++ Buildplan/Docker/startup.sh
@@ -7,12 +7,11 @@ export MYSQL_BASE="wp"
 
 export USER=$(whoami)
 
-cd "${WORKSPACE}/tests"
-
 chown -R mysql:mysql /var/lib/mysql
 service mysql start
 
 echo "Create database $MYSQL_BASE;" > /tmp/script.sql
 mysql -u$MYSQL_USER -p$MYSQL_PASS < /tmp/script.sql
 
+cd tests
 sudo /bin/bash ./runIntegrationTests.sh --oauth="$GITHUB_OAUTH" --db-host="$MYSQL_HOST" --db-user="$MYSQL_USER" --db-pass="$MYSQL_PASS" --db-name="$MYSQL_BASE" --project-id="$PROJECT_ID" --user-ident="$USER_IDENTIFIER" --token-secret="$TOKEN_SECRET"
