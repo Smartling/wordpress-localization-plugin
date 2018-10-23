fac74aa7f2187152393912d70d30126739730132
Pavlo Ratushnyi <pratushnyi@smartling.com>
2018-10-24 01:01:36 +0300

    fixed firebase connection issue

--- js/progress-tracker.js
+++ js/progress-tracker.js
@@ -1,239 +1,196 @@
 (function ($) {
-    $(document).ready(function () {
-        var NotificationsManager = function (firebase, data, appName, recordId) {
-            this.data = data;
-            this.spaceId = window.firebaseIds.space_id;
-            this.objectId = window.firebaseIds.object_id;
-
-            if (data.config && data.token) {
-                firebase.initializeApp(data.config, appName);
-                firebase.auth().onAuthStateChanged(function (user) {
-                    if (!user) {
-                        firebase.auth().signInWithCustomToken(data.token);
-                    }
-                });
-            }
-
-            var thisContext = this;
-
-            this.listen = function (event, callback) {
-                firebase.database().ref()
-                    .child("accounts")
-                    .child(this.data.accountUid)
-                    .child("projects")
-                    .child(this.data.projectId)
-                    .child(this.spaceId)
-                    .child(this.objectId)
-                    .child(recordId).on(event, function (snap) {
-                    callback(snap, thisContext);
-                });
-
-                return this;
-            };
-
-            this.deleteRecord = function (recordId) {
-                $.post(window.deleteNotificationEndpoint, {
-                    project_id: this.data.projectId,
-                    space_id: this.spaceId,
-                    object_id: this.object_id,
-                    record_id: recordId
-                }, function (data) {
-                    console.log(data);
-                });
-            }
-        };
-
-        if (!Object.prototype.hasOwnProperty.call(window, 'firebaseConfig')
-            || !Object.prototype.hasOwnProperty.call(window, 'recordId')) {
-            return;
-        }
-
-        var wrapperBlock = `<div class="${window.notificationClassName}"></div>`;
-        $("body").append(wrapperBlock);
-
-        Object.keys(window.firebaseConfig).forEach(function (e) {
-            var notificationManager = new NotificationsManager(
-                firebase,
-                window.firebaseConfig[e],
-                e == 0 ? "[DEFAULT]" : Math.random().toString(),
-                recordId
-            );
-
-            var getBoxId = function (id) {
-                return `box_${id}`;
-            }
-
-            var placeBox = function (id) {
-                if (0 === $(`#${getBoxId(id)}`).length) {
-                    var $wrapper = $(`.${window.notificationClassName}`);
-                    $($wrapper).append(`<div id="${getBoxId(id)}" class="notification-message-box"></div>`);
-                    $message = $(`#${getBoxId(id)}`);
-                    $message.on("click", function () {
-                        notificationManager.deleteRecord(recordId);
-                        $message.slideUp(100, function () {
-                            $message.remove();
-                        });
-                    });
-                    $message.slideDown(100);
-                }
-            };
-
-            var setMessage = function (id, severity, message) {
-                var cssClass = `notification-message-box ${severity}`;
-                $(`#${getBoxId(id)}`).attr("class", cssClass);
-                $(`#${getBoxId(id)}`).html(message);
-            }
-
-            notificationManager
-                .listen("child_changed", function (snap, notificationManager) {
-                    placeBox(recordId);
-                    var messageData = snap.val();
-                    if (Object.prototype.hasOwnProperty.call(messageData, "severity")
-                        && Object.prototype.hasOwnProperty.call(messageData, "message")) {
-                        setMessage(recordId, messageData.severity, messageData.message);
-                    }
-                })
-                .listen("child_removed", function (snap) {
-                    var $target = $(`#${getBoxId((recordId))}`);
-                    $target.slideUp(100, function () {
-                        $target.remove();
-                    });
-                });
-        });
-    });
 
     $(document).ready(function () {
-        var NotificationsManagerGeneral = function (firebase, data, appName) {
-            this.data = data;
-            this.spaceId = window.firebaseIds.space_id;
-            this.objectId = window.firebaseIds.object_id;
-
-            if (data.config && data.token) {
-                firebase.initializeApp(data.config, appName);
-                firebase.auth().onAuthStateChanged(function (user) {
-                    if (!user) {
-                        firebase.auth().signInWithCustomToken(data.token);
-                    }
-                });
-            }
-
-            var thisContext = this;
-
-            this.listen = function (event, callback) {
-                firebase.database().ref()
-                    .child("accounts")
-                    .child(this.data.accountUid)
-                    .child("projects")
-                    .child(this.data.projectId)
-                    .child(this.spaceId)
-                    .child(this.objectId).on(event, function (snap) {
-                    callback(snap, thisContext);
-                });
-
-                return this;
-            };
-
-            this.deleteRecord = function (recordId) {
-                $.post(window.deleteNotificationEndpoint, {
-                    project_id: this.data.projectId,
-                    space_id: this.spaceId,
-                    object_id: this.object_id,
-                    record_id: recordId
-                }, function (data) {
-                    console.log(data);
-                });
-            }
-        };
 
         if (!Object.prototype.hasOwnProperty.call(window, 'firebaseConfig')) {
             return;
         }
 
-        var wrapperBlock = `<div class="${window.notificationClassNameGeneral} hidden"></div>`;
+        var NotificationsManagerGeneric = function (account, project, space, object, record) {
+            this.accountUid = account;
+            this.projectId = project;
+            this.spaceId = space;
+            this.objectId = object;
+            this.recordId = record;
 
-        $("body").append(wrapperBlock);
+            var thisContext = this;
 
-        Object.keys(window.firebaseConfig).forEach(function (e) {
-            var notificationManager = new NotificationsManagerGeneral(
-                firebase,
-                window.firebaseConfig[e],
-                e == 0 ? "[DEFAULT]" : Math.random().toString()
-            );
+            var getObjectPath = function (c) {
+                return `accounts/${c.accountUid}/projects/${c.projectId}/${c.spaceId}/${c.objectId}`;
+            };
+
+            var getRecordPath = function (c) {
+                return `${getObjectPath(c)}/${c.recordId}`;
+            }
+
+            this.listenObject = function (event, callback) {
+                firebase.database().ref(getObjectPath(thisContext)).on(event, function (snap) {
+                    callback(snap, thisContext);
+                })
+            };
+
+            this.listenRecord = function (event, callback) {
+                firebase.database().ref(getRecordPath(thisContext)).on(event, function (snap) {
+                    callback(snap, thisContext);
+                })
+            };
+
+            this.deleteRecord = function (recordId) {
+                $.post(window.deleteNotificationEndpoint, {
+                    project_id: this.data.projectId,
+                    space_id: this.spaceId,
+                    object_id: this.object_id,
+                    record_id: recordId
+                }, function (data) {
+                    console.log(data);
+                });
+            }
+        };
+
+        var eventAnimate = function () {
+            $('li.smartling-live-menu span.circle')
+                .animate({backgroundColor: "#FF3333"}, 1) /* event color */
+                .delay(300)
+                .animate({backgroundColor: "#888a85"}, 700);
+        };
+
+        var objectNotificationWrapperBlock = `<div class="${window.notificationClassNameGeneral} hidden"></div>`;
+        $("body").append(objectNotificationWrapperBlock);
+
+        var recordNotificationWrapperBlock = `<div class="${window.notificationClassName}"></div>`;
+        $("body").append(recordNotificationWrapperBlock);
+
+        var connectToFirebase = function (data, appName) {
+            if (data.config && data.token) {
+                firebase.initializeApp(data.config, appName);
+                firebase.auth().onAuthStateChanged(function (user) {
+                    if (!user) {
+                        firebase.auth().signInWithCustomToken(data.token);
+                    }
+                });
+            }
+        };
+
+        var setMessage = function (messageBox, severity, message, recordId) {
+            var cssClass = `notification-message-box ${severity}`;
+            $(`#${messageBox}`).attr("class", cssClass);
+            $(`#${messageBox}`).attr("data-record-id", recordId);
+            $(`#${messageBox}`).html(message);
+
+            setTimeout(function () {
+                try {
+                    jQuery(`div.notification-message-box [data-record-id=${recordId}]`).remove();
+                } catch (e) {
+
+                }
+            }, 10000);
+
+        };
+
+        var validateData = function (data) {
+            return Object.prototype.hasOwnProperty.call(data, "severity")
+                && Object.prototype.hasOwnProperty.call(data, "message");
+        };
+
+        var placeBox = function (wrapper, id, manager) {
+            var msgId = `#${id}`;
+            if (0 === $(msgId).length) {
+                $(`.${wrapper}`).append(`<div id="${id}" class="notification-message-box"></div>`);
+                var $message = $(msgId);
+                $message.on("click", function () {
+                    var recordId = $(this).attr("data-record-id");
+                    manager.deleteRecord(recordId);
+                    $message.slideUp(100, function () {
+                        $message.remove();
+                    });
+                });
+                $message.slideDown(100);
+            }
+        };
+
+        var addObjectHandlers = function (manager, recordId) {
 
             var messageBoxSelector = 'smartling_notification_general';
 
-            var placeBox = function () {
-                if (0 === $(`#${messageBoxSelector}`).length) {
-                    var $wrapper = $(`.${window.notificationClassNameGeneral}`);
-                    $($wrapper).append(`<div id="${messageBoxSelector}" class="notification-message-box"></div>`);
-                    var $message = $(`#${messageBoxSelector}`);
-                    $message.on("click", function () {
-                        var recordId = $(this).attr("data-record-id");
-                        notificationManager.deleteRecord(recordId);
-                        $message.slideUp(100, function () {
-                            $message.remove();
-                        });
-                    });
-                    $message.slideDown(100);
+            manager.listenObject("child_changed", function (snap, notificationManager) {
+                eventAnimate();
+                placeBox(window.notificationClassNameGeneral, messageBoxSelector, notificationManager);
+                var messageData = snap.val();
+                if (validateData(messageData.data) && recordId !== snap.key) {
+                    setMessage(messageBoxSelector, messageData.data.severity, messageData.data.message, snap.key);
                 }
-            };
+            });
 
-            var setMessage = function (severity, message, recordId) {
-                var cssClass = `notification-message-box ${severity}`;
-                $(`#${messageBoxSelector}`).attr("class", cssClass);
-                $(`#${messageBoxSelector}`).attr("data-record-id", recordId);
-                $(`#${messageBoxSelector}`).html(message);
-
-                setTimeout(function () {
-                    clearNotification(recordId);
-                }, 10000);
-
-            }
-
-            var clearNotification = function (recordId) {
-                var el = $(`#${messageBoxSelector} [data-record-id=${recordId}]`);
-                if (0 !== el.length) {
-                    $(el[0]).remove();
-                }
-
-                if (0 === $(`.${window.notificationClassNameGeneral} div`).length) {
-                    $(`.${window.notificationClassNameGeneral}`).toggleClass('hidden');
-                }
-            };
-
-            notificationManager
-                .listen("child_changed", function (snap, notificationManager) {
-                    eventAnimate();
-                    placeBox();
-                    var messageData = snap.val();
-
-                    if (Object.prototype.hasOwnProperty.call(messageData.data, "severity")
-                        && Object.prototype.hasOwnProperty.call(messageData.data, "message")) {
-                        setMessage(messageData.data.severity, messageData.data.message, snap.key);
-                    }
-                })
-                .listen("child_removed", function (snap) {
-                    eventAnimate();
-                    var $target = $(`messageBoxSelector [data-record-id=${snap.key}]`);
-                    $target.slideUp(100, function () {
+            manager.listenObject("child_removed", function (snap) {
+                eventAnimate();
+                var $target = $(`messageBoxSelector [data-record-id=${snap.key}]`);
+                $target.slideUp(100, function () {
+                    try {
+                        var wrapper = $target.parent();
                         $target.remove();
-                        clearNotification(snap.key);
-                    });
-                });
-        });
 
-        var eventAnimate = function () {
-            jQuery('li.smartling-live-menu span.circle')
-                .animate({backgroundColor: "#FF3333"}, 1) /* event color */
-                .delay(300).animate({backgroundColor: "#888a85"}, 700);
+                        if (0 === jQuery(`.${window.notificationClassNameGeneral} div`).length
+                            && !$jQuery(wrapper).hasClass('hidden')) {
+                            jQuery(wrapper).toggleClass('hidden');
+                        }
+                    } catch (e) {
+                    }
+                });
+            })
         };
+
+        var addRecordHandlers = function (manager, recordId) {
+
+            var getBoxId = function (id) {
+                return `box_${id}`;
+            };
+
+            manager.listenRecord("child_changed", function (snap, notificationManager) {
+                placeBox(window.notificationClassName, getBoxId(recordId), notificationManager);
+                var messageData = snap.val();
+                if (validateData(messageData)) {
+                    var messageBox = getBoxId(recordId);
+                    setMessage(messageBox, messageData.severity, messageData.message, recordId);
+                }
+            });
+
+            manager.listenRecord("child_removed", function (snap, notificationManager) {
+                var $target = $(`#${getBoxId(recordId)}`);
+                $target.slideUp(100, function () {
+                    $target.remove();
+                });
+            });
+        };
+
+        var initNotificationManagers = function (data) {
+            var recordId = Object.prototype.hasOwnProperty.call(window, 'recordId') ? window.recordId : undefined;
+
+            var manager = new NotificationsManagerGeneric(
+                data.accountUid,
+                data.projectId,
+                window.firebaseIds.space_id,
+                window.firebaseIds.object_id,
+                recordId
+            );
+
+            addObjectHandlers(manager, recordId);
+
+            if (undefined !== recordId) {
+                addRecordHandlers(manager, recordId);
+            }
+        };
+
+        Object.keys(window.firebaseConfig).forEach(function (el) {
+            var appName = el == 0 ? "[DEFAULT]" : el;
+            var data = window.firebaseConfig[el];
+
+            connectToFirebase(data, appName);
+            initNotificationManagers(data);
+        });
 
         $('.smartling-live-menu').on('click', function (e) {
             var identifier = `.${window.notificationClassNameGeneral}`;
             $(identifier).toggleClass('hidden');
         });
     });
-
-
-
 })(jQuery);
