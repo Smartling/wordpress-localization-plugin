0f97d917971c62759125880a867043ef18d84630
Pavlo Ratushnyi <pratushnyi@smartling.com>
2019-04-05 11:03:36 +0300

    tests

--- tests/IntegrationTests/tests/SubmissionCleanupTest.php
+++ tests/IntegrationTests/tests/SubmissionCleanupTest.php
@@ -2,6 +2,7 @@
 
 namespace Smartling\Tests\IntegrationTests\tests;
 
+use Smartling\Helpers\SimpleStorageHelper;
 use Smartling\Submissions\SubmissionEntity;
 use Smartling\Tests\IntegrationTests\SmartlingUnitTestCaseAbstract;
 
@@ -48,26 +49,36 @@ class SubmissionCleanupTest extends SmartlingUnitTestCaseAbstract
         $this->assertTrue(0 === count($submissions));
     }
 
+    private function prepareTempFile(SubmissionEntity $submission)
+    {
+        file_put_contents(
+            DIR_TESTDATA . '/test.php',
+            vsprintf(
+                '<?php wp_delete_post(%s, true);',
+                [
+                    $submission->getSourceId(),
+                ]
+            )
+        );
+
+        SimpleStorageHelper::set('execFile', DIR_TESTDATA . '/test.php');
+    }
+
     public function testRemovedOriginalContent()
     {
         $submission = $this->makePreparations();
-        $tmpFile = DIR_TESTDATA . '/test.php';
-        file_put_contents($tmpFile, vsprintf('<?php wp_delete_post(%s, true);', [$submission->getSourceId()]));
-        $this->wpcli_exec('package', 'install', 'wp-cli/profile-command');
-        $this->wpcli_exec('profile', 'eval-file', $tmpFile);
-        unlink($tmpFile);
+        $this->prepareTempFile($submission);
+
+        $this->wpcli_exec('cron', 'event', 'run exec_plugin_execute_hook');
         $this->checkSubmissionIsRemoved();
     }
 
     public function testRemovedTranslatedContent()
     {
         $submission = $this->makePreparations();
-        $tmpFile = DIR_TESTDATA . '/test.php';
-        file_put_contents($tmpFile, vsprintf('<?php switch_to_blog(%s); wp_delete_post(%s, true);', [$submission->getTargetBlogId(),
-                                                                                                     $submission->getTargetId()]));
-        $this->wpcli_exec('package', 'install', 'wp-cli/profile-command');
-        $this->wpcli_exec('profile', 'eval-file', $tmpFile);
-        unlink($tmpFile);
+        $this->prepareTempFile($submission);
+
+        $this->wpcli_exec('cron', 'event', 'run exec_plugin_execute_hook');
         $this->checkSubmissionIsRemoved();
     }
 }
