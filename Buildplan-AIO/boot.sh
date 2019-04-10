304d0a2fc8b92d30d7184df345468354a5f28dee
Pavlo Ratushnyi <pratushnyi@smartling.com>
2019-04-10 21:34:59 +0300

    working on new build plan

---
+++ Buildplan-AIO/boot.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+
+
+#COPY smartling-connector-dev ${PLUGIN_DIR}
+
+#RUN ln -s "${PLUGIN_DIR}/tests/IntegrationTests/testdata/acf-pro-test-definitions" "${WP_PLUGINS_DIR}/acf-pro-test-definitions"
+#RUN ln -s "${PLUGIN_DIR}/tests/IntegrationTests/testdata/exec-plugin" "${WP_PLUGINS_DIR}/exec-plugin"
+
+#RUN chown -R mysql:mysql /var/lib/mysql && service mysql start && \
+#    ${WPCLI} plugin activate \
+#    exec-plugin \
+#    advanced-custom-fields-pro \
+#    --network
+
+
+
+ls -lah /
+
