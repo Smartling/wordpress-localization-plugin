3d1c5c6ef099dd38541885eb5a95781417b5d42f
Vitalii Solovei <vsolovei@smartling.com>
2021-11-04 13:13:24 +0200

    add delete attachment hook, change test (WP-664)

--- inc/Smartling/Helpers/SubmissionCleanupHelper.php
+++ inc/Smartling/Helpers/SubmissionCleanupHelper.php
@@ -37,6 +37,7 @@ class SubmissionCleanupHelper implements WPHookInterface
     {
         add_action('before_delete_post', [$this, 'beforeDeletePostHandler']);
         add_action('pre_delete_term', [$this, 'preDeleteTermHandler'], 999, 2);
+        add_action('delete_attachment', [$this, 'deleteAttachmentHandler'], 10, 2);
     }
 
     /**
@@ -59,9 +60,22 @@ class SubmissionCleanupHelper implements WPHookInterface
             $post_type = get_post($postId)->post_type;
         }
 
-        $this->lookForSubmissions($post_type, $currentBlogId, (int)$postId);
+        $this->deleteSubmissions($post_type, $currentBlogId, (int)$postId);
 
         add_action('before_delete_post', [$this, 'beforeDeletePostHandler']);
+    }
+
+    /**
+     * @param int $postId
+     * @param \WP_Post $post
+     * @noinspection PhpMissingParamTypeInspection called by WordPress, not sure if typed
+     */
+    public function deleteAttachmentHandler($postId, $post): void
+    {
+        $postType = $post->post_type ?? 'attachment';
+        $currentBlogId = $this->siteHelper->getCurrentBlogId();
+        $this->getLogger()->debug("Attachment id=$postId type=$postType in blogId=$currentBlogId is going to be deleted");
+        $this->deleteSubmissions($postType, $currentBlogId, (int)$postId);
     }
 
     /**
@@ -85,13 +99,13 @@ class SubmissionCleanupHelper implements WPHookInterface
         );
 
         try {
-            $this->lookForSubmissions($taxonomy, $currentBlogId, (int)$term);
+            $this->deleteSubmissions($taxonomy, $currentBlogId, (int)$term);
         } catch (\Exception $e) {
             $this->getLogger()->warning($e->getMessage());
         }
     }
 
-    private function lookForSubmissions(string $contentType, int $blogId, int $contentId): void
+    private function deleteSubmissions(string $contentType, int $blogId, int $contentId): void
     {
         // try treat as translation
         $this->processDeletion([
@@ -119,8 +133,7 @@ class SubmissionCleanupHelper implements WPHookInterface
         if (0 < count($submissions)) {
             $this->getLogger()->debug(vsprintf('Found %d submissions', [count($submissions)]));
             foreach ($submissions as $submission) {
-                $this->unlinkContent($submission);
-                $this->submissionManager->delete($submission);
+                $this->deleteSubmission($submission);
             }
         } else {
             $this->getLogger()
@@ -128,10 +141,19 @@ class SubmissionCleanupHelper implements WPHookInterface
         }
     }
 
-    /**
-     * @param SubmissionEntity $submission
-     */
-    private function unlinkContent(SubmissionEntity $submission)
+    public function deleteSubmission(SubmissionEntity $submission): void
+    {
+        $this->unlinkContent($submission);
+        $submission->setStatus(SubmissionEntity::SUBMISSION_STATUS_DELETED);
+        $submission->setLastModified(new \DateTime());
+        $this->submissionManager->storeEntity($submission);
+        if ('' !== $submission->getStateFieldFileUri() && 0 === $this->submissionManager->find([SubmissionEntity::FIELD_FILE_URI => $submission->getFileUri()])) {
+            $this->getLogger()->debug("File {$submission->getFileUri()} is not in use and will be deleted");
+            $this->apiWrapper->deleteFile($submission);
+        }
+    }
+
+    private function unlinkContent(SubmissionEntity $submission): void
     {
         $result = false;
         $this->getLogger()->debug(
