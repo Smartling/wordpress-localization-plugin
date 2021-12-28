93f6828d10a3266e5ceef6f106874c62f84f3fca
Vitalii Solovei <vsolovei@smartling.com>
2021-12-28 08:36:08 +0200

    use lib from deployed (WP-678)

--- Buildplan/build.sh
+++ Buildplan/build.sh
@@ -18,30 +18,12 @@
 # CRE_TOKEN_SECRET
 
 # install composer
-COMPOSER_INSTALL_DIR="$LOCAL_GIT_DIR/inc/third-party/bin"
-if [ ! -d "$COMPOSER_INSTALL_DIR" ]; then
-    mkdir -p "$COMPOSER_INSTALL_DIR"
-fi
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php --install-dir="$COMPOSER_INSTALL_DIR" --filename=composer
-php -r "unlink('composer-setup.php');"
-COMPOSER_BIN="$COMPOSER_INSTALL_DIR/composer"
-cp $COMPOSER_INSTALL_DIR/composer /usr/local/bin/composer
-composer global require ilab/namespacer
+svn checkout https://plugins.svn.wordpress.org/smartling-connector/trunk trunk
+cp -r trunk/inc/lib ./inc
 
 chown -R mysql:mysql /var/lib/mysql && service mysql start
 
 cd "$LOCAL_GIT_DIR"
-chdir
-curl -s -o ~/.composer/vendor/ilab/namespacer/Source/Commands/RenamespaceCommand.php https://raw.githubusercontent.com/vsolovei-smartling/namespacer/master/Source/Commands/RenamespaceCommand.php
-curl -s -o ~/.composer/vendor/ilab/namespacer/Source/Models/Package.php https://raw.githubusercontent.com/vsolovei-smartling/namespacer/master/Source/Models/Package.php
-curl -s -o ~/.composer/vendor/ilab/namespacer/Source/Models/Project.php  https://raw.githubusercontent.com/vsolovei-smartling/namespacer/master/Source/Models/Project.php
-
-composer update
-~/.composer/vendor/ilab/namespacer/bin/namespacer --composer ./composer.json --package smartling-connector --namespace "Smartling\Vendor" inc
-# two instances of replace not a typo
-find ./inc/lib -type f -name '*.php' -exec sed -i '' 's~Smartling\\Vendor\\Smartling\\Vendor\\~Smartling\\Vendor\\~g' {} +
-find ./inc/lib -type f -name '*.php' -exec sed -i '' 's~Smartling\\Vendor\\Smartling\\Vendor\\~Smartling\\Vendor\\~g' {} +
 
 # remove installer plugin dir and replace with dev dir
 rm -rf "$PLUGIN_DIR"
@@ -67,14 +49,10 @@ chmod +x $PHPUNIT_BIN
 
 PHPUNIT_XML="${PLUGIN_DIR}/tests/phpunit.xml"
 
-#${PHPUNIT_BIN} -c ${PHPUNIT_XML}
+${PHPUNIT_BIN} -c ${PHPUNIT_XML}
 
 PHPUNIT_EXIT_CODE=$?
 
 service mysql stop
 
-#svn checkout https://plugins.svn.wordpress.org/smartling-connector/trunk trunk
-
 chdir
-
-zip -r wordpress-connector.zip trunk/
