7facc9e67657539315067eb46bb197846f049f5c
Vitalii Solovei <vsolovei@smartling.com>
2022-01-25 19:57:41 +0200

    add sleep

--- Buildplan/release.sh
+++ Buildplan/release.sh
@@ -4,14 +4,15 @@ svn -q checkout https://plugins.svn.wordpress.org/smartling-connector/trunk trun
 cd trunk
 ls -la
 cp ../readme.txt ../smartling-connector.php .
-cp -r ../css ./css
-cp -r ../js ./js
-cp -r ../languages ./languages
+cp -r ../css .
 cp -r ../inc/config ./inc
 cp -r ../inc/Smartling ./inc
+cp -r ../js .
+cp -r ../languages .
 svn add --force * --auto-props --parents --depth infinity -q
 TAG=`grep '* Version' smartling-connector.php | sed 's/ \\* Version: *//'`
-echo $TAG
 svn status
+echo "About to commit $TAG in a minute!"
+sleep 1m
 #svn commit -m 'Update to v $TAG' --username smartling --password $WORDPRESS_ORG_SVN_PASSWORD
 #svn copy https://plugins.svn.wordpress.org/smartling-connector/trunk https://plugins.svn.wordpress.org/smartling-connector/tags/\$TAG -m 'Tagging new version $TAG'
