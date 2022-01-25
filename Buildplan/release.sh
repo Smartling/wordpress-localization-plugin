23cc5871ec69161845b67ce47273df18e37e38ef
Vitalii Solovei <vsolovei@smartling.com>
2022-01-25 20:30:43 +0200

    fix tag

--- Buildplan/release.sh
+++ Buildplan/release.sh
@@ -13,5 +13,5 @@ TAG=`grep '* Version' smartling-connector.php | sed 's/ \\* Version: *//'`
 svn status
 echo "About to commit $TAG with these changes in a minute!"
 sleep 1m
-svn commit -m 'Update to v $TAG' --username smartling --password $WORDPRESS_ORG_SVN_PASSWORD
-svn copy https://plugins.svn.wordpress.org/smartling-connector/trunk https://plugins.svn.wordpress.org/smartling-connector/tags/$TAG -m 'Tagging new version $TAG' --username smartling --password $WORDPRESS_ORG_SVN_PASSWORD
+svn commit -m "Update to v $TAG" --username smartling --password $WORDPRESS_ORG_SVN_PASSWORD
+svn copy https://plugins.svn.wordpress.org/smartling-connector/trunk https://plugins.svn.wordpress.org/smartling-connector/tags/$TAG -m "Tagging new version $TAG" --username smartling --password $WORDPRESS_ORG_SVN_PASSWORD
