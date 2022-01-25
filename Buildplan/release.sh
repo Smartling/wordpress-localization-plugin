dc1b53ddbbada79e35f323d5584c04de5584ccd7
Vitalii Solovei <vsolovei@smartling.com>
2022-01-25 20:13:06 +0200

    uncomment commit steps

--- Buildplan/release.sh
+++ Buildplan/release.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-svn --version
 svn -q checkout https://plugins.svn.wordpress.org/smartling-connector/trunk trunk
 cd trunk
 ls -la
@@ -14,5 +13,5 @@ TAG=`grep '* Version' smartling-connector.php | sed 's/ \\* Version: *//'`
 svn status
 echo "About to commit $TAG in a minute!"
 sleep 1m
-#svn commit -m 'Update to v $TAG' --username smartling --password $WORDPRESS_ORG_SVN_PASSWORD
-#svn copy https://plugins.svn.wordpress.org/smartling-connector/trunk https://plugins.svn.wordpress.org/smartling-connector/tags/\$TAG -m 'Tagging new version $TAG'
+svn commit -m 'Update to v $TAG' --username smartling --password $WORDPRESS_ORG_SVN_PASSWORD
+svn copy https://plugins.svn.wordpress.org/smartling-connector/trunk https://plugins.svn.wordpress.org/smartling-connector/tags/$TAG -m 'Tagging new version $TAG'
