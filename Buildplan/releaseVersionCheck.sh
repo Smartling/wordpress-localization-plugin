e525078872978df77a8003a315a134b9ffa2c545
Vitalii Solovei <vsolovei@smartling.com>
2022-05-12 15:40:25 +0200

    debug (WP-712)

--- Buildplan/releaseVersionCheck.sh
+++ Buildplan/releaseVersionCheck.sh
@@ -3,6 +3,6 @@ cd trunk
 CONNECTOR_VERSION=`grep '* Version' smartling-connector.php | sed 's/ \\* Version: *//'`
 COMPOSER_VERSION=`grep '"version"' composer.json | sed 's/  "version": "(\d+\.\d+\.\d+)"*/\1/'`
 STABLE_TAG=`grep 'Stable tag:' readme.txt | sed 's/Stable tag: //'`
-echo CONNECTOR_VERSION
-echo COMPOSER_VERSION
-echo STABLE_TAG
+echo $CONNECTOR_VERSION
+echo $COMPOSER_VERSION
+echo $STABLE_TAG
