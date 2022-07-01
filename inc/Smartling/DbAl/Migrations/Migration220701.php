5f8ae4bb004bf70e1bb66829637ad0a698b39d71
Vitalii Solovei <vsolovei@smartling.com>
2022-07-01 15:23:37 +0200

    add created at field (WP-639)

---
+++ inc/Smartling/DbAl/Migrations/Migration220701.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Smartling\DbAl\Migrations;
+
+use Smartling\Base\SmartlingEntityAbstract;
+use Smartling\Submissions\SubmissionEntity;
+
+class Migration220701 implements SmartlingDbMigrationInterface
+{
+    public function getVersion(): int
+    {
+        return 220701;
+    }
+
+    public function getQueries($tablePrefix = 'wp_'): array
+    {
+        $tableName = SubmissionEntity::getTableName();
+        $columnName = SubmissionEntity::FIELD_CREATED_AT;
+        return [
+            "ALTER TABLE $tableName ADD $columnName " . SmartlingEntityAbstract::DB_TYPE_DATETIME,
+            "UPDATE $tableName SET $columnName = " . SubmissionEntity::FIELD_SUBMISSION_DATE,
+        ];
+    }
+}
