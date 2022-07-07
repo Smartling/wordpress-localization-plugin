d2c7a4ff91b765e24ed86f73dc13811f79f8198f
Vitalii Solovei <vsolovei@smartling.com>
2022-07-07 15:07:34 +0200

    remove unused (WP-736)

--- inc/Smartling/Helpers/PlaceholderHelper.php
+++ inc/Smartling/Helpers/PlaceholderHelper.php
@@ -6,9 +6,4 @@ class PlaceholderHelper
 {
     public const SMARTLING_PLACEHOLDER_MASK_START = '#sl-start#';
     public const SMARTLING_PLACEHOLDER_MASK_END = '#sl-end#';
-
-    public function buildPlaceholder($content): string
-    {
-        return self::SMARTLING_PLACEHOLDER_MASK_START . $content . self::SMARTLING_PLACEHOLDER_MASK_END;
-    }
 }
