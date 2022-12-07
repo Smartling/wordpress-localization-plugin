6158177b22c2c130b102aab2f98274f4f5cb3e1a
Vitalii Solovei <vsolovei@smartling.com>
2022-12-07 14:49:30 +0100

    cleanup (WP-779)

--- tests/IntegrationTests/tests/PlaceholdersTest.php
+++ tests/IntegrationTests/tests/PlaceholdersTest.php
@@ -36,44 +36,4 @@ HTML;
             var_dump(get_post_meta($submission->getTargetId(), $metaKey, true));
         });
     }
-
-    /**
-     * Submit cloned post with attached category which has parent category.
-     * Attached only child category but not parent.
-     * Expected result: 3 cloned submissions in "Completed" state
-     * (1 post, 2 categories).
-     */
-    public function testSubmitClonedPostWithCategoryWhichHasParentCategory()
-    {
-        $rootCategoryId = $this->createTerm('Category A');
-        $childCategoryId = $this->createTerm('Category B');
-        $postId = $this->createPost();
-        $this->makeRelationBetweenTerms($rootCategoryId, $childCategoryId);
-        $this->addTaxonomyToPost($postId, $childCategoryId);
-
-        $translationHelper = $this->getTranslationHelper();
-        $submission = $translationHelper->prepareSubmission('post', 1, $postId, 2, true);
-
-        self::assertSame(SubmissionEntity::SUBMISSION_STATUS_NEW, $submission->getStatus());
-        self::assertSame(1, $submission->getIsCloned());
-
-        $this->uploadDownload($submission);
-
-        $submissions = $this->getSubmissionManager()->find(
-            [
-                'content_type' => 'category',
-                // Status will be "Completed" because of an issue in
-                // SubmissionEntity::getCompletionPercentage() method. It
-                // returns 1 (100%) when total string count and excluded string
-                // count equal 0.
-                // TODO: fix SubmissionEntity::getCompletionPercentage() and
-                // fix this test then (replace SUBMISSION_STATUS_COMPLETED with
-                // SUBMISSION_STATUS_IN_PROGRESS).
-                'status'       => SubmissionEntity::SUBMISSION_STATUS_COMPLETED,
-                'is_cloned'    => 1,
-            ]
-        );
-
-        self::assertCount(2, $submissions);
-    }
 }
