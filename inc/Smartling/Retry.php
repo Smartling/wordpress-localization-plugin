991baa036ab7cc844f6f6a45d412f1db5e771ce7
Vitalii Solovei <vsolovei@smartling.com>
2023-09-06 12:16:19 +0200

    add attribute target (WP-828)

--- inc/Smartling/Retry.php
+++ inc/Smartling/Retry.php
@@ -4,7 +4,7 @@ namespace Smartling;
 
 use Attribute;
 
-#[Attribute]
+#[Attribute(Attribute::TARGET_FUNCTION|Attribute::TARGET_METHOD)]
 class Retry {
     public function __construct(public int $retryAttempts = 4, public int $retryTimeoutSeconds = 1)
     {
