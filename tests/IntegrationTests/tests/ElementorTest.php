1b0d44f37fb32e6d0ea0714bf4e72eda444d33c7
Vitalii Solovei <vsolovei@smartling.com>
2023-11-23 16:05:54 +0100

    refactor test (WP-847)

--- tests/IntegrationTests/tests/ElementorTest.php
+++ tests/IntegrationTests/tests/ElementorTest.php
@@ -9,10 +9,11 @@ class ElementorTest extends SmartlingUnitTestCaseAbstract {
         $contentType = 'post';
         $imageIds = [$this->createAttachment(), $this->createAttachment()];
         $postId = $this->createPostWithMeta('Elementor automated test post', '', $contentType, [
-            '_elementor_data' => addslashes(<<<JSON
-[{"id":"590657a","elType":"section","settings":{"structure":"30"},"elements":[{"id":"b56da21","elType":"column","settings":{"_column_size":33,"_inline_size":null},"elements":[{"id":"c799791","elType":"widget","settings":{"editor":"<p>Left text three six seven<\/p>"},"elements":[],"widgetType":"text-editor"}],"isInner":false},{"id":"0f3ad3c","elType":"column","settings":{"_column_size":33,"_inline_size":null},"elements":[{"id":"0088b31","elType":"widget","settings":{"editor":"<p>Middle text\u00a0<span style=\"color: var( --e-global-color-text ); font-family: var( --e-global-typography-text-font-family ), Sans-serif, serif; font-weight: var( --e-global-typography-text-font-weight ); font-size: 2.1rem;\">three six seven<\/span><\/p>"},"elements":[],"widgetType":"text-editor"}],"isInner":false},{"id":"8798127","elType":"column","settings":{"_column_size":33,"_inline_size":null},"elements":[{"id":"78d53a1","elType":"widget","settings":{"title":"Right heading &nbsp;three six seven"},"elements":[],"widgetType":"heading"}],"isInner":false}],"isInner":false},{"id":"7a874c7","elType":"section","settings":[],"elements":[{"id":"d7d603e","elType":"column","settings":{"_column_size":100,"_inline_size":null},"elements":[{"id":"ea10188","elType":"widget","settings":{"image":{"url":"http:\/\/example.com\/wp-content\/uploads\/2023\/06\/elementor-image.png","id":$imageIds[0],"alt":"","source":"library"},"image_size":"medium"},"elements":[],"widgetType":"image"}],"isInner":false}],"isInner":false},{"id":"6334fe1","elType":"section","settings":[],"elements":[{"id":"27c6d9f","elType":"column","settings":{"_column_size":100,"_inline_size":null},"elements":[{"id":"d9ecb93","elType":"widget","settings":{"image":{"url":"http:\/\/example.com\/wp-content\/uploads\/2023\/06\/20220726-1.jpeg","id":$imageIds[1],"alt":"","source":"library"}},"elements":[],"widgetType":"image"}],"isInner":false}],"isInner":false}]
-JSON
-            ),
+            '_elementor_data' => addslashes(sprintf(
+                str_replace('"%d"', '%d', file_get_contents(__DIR__ . '/../testdata/elementor.json')),
+                $imageIds[0],
+                $imageIds[1],
+            )),
         ]);
         $sourceBlogId = 1;
         $targetBlogId = 2;
