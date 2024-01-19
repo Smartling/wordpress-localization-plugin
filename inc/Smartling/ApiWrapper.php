214d7320fe6f107dc52c942d6403c7f139582dc8
Vitalii Solovei <vsolovei@smartling.com>
2024-01-19 11:01:11 +0100

    use wordpress date and time formats for daily bucket jobs (WP-867)

--- inc/Smartling/ApiWrapper.php
+++ inc/Smartling/ApiWrapper.php
@@ -547,9 +547,7 @@ class ApiWrapper implements ApiWrapperInterface
 
     private function getBaseNameForDailyBucketJob(string $suffix = ''): string
     {
-        $date = DateTimeHelper::getWordpressDateFormat();
-
-        return ApiWrapperInterface::DAILY_BUCKET_JOB_NAME_PREFIX . " $date$suffix";
+        return ApiWrapperInterface::DAILY_BUCKET_JOB_NAME_PREFIX . ' ' . date(DateTimeHelper::getWordpressDateFormat()) . $suffix;
     }
 
     public function findLastJobByFileUri(ConfigurationProfileEntity $profile, string $fileUri): ?JobEntityWithStatus
@@ -593,7 +591,7 @@ class ApiWrapper implements ApiWrapperInterface
                     // If there is a CLOSED bucket job then we have to
                     // come up with new job name in order to avoid
                     // "Job name is already taken" error.
-                    $jobName = $this->getBaseNameForDailyBucketJob(' ' . DateTimeHelper::getWordpressTimeFormat());
+                    $jobName = $this->getBaseNameForDailyBucketJob(' ' . date(DateTimeHelper::getWordpressTimeFormat()));
                     $result = $this->createJob($profile, [
                         'name'        => $jobName,
                         'description' => 'Bucket job: contains updated content.',
