58503b14f73751fd222f133538546a40b631a583
Vitalii Solovei <vsolovei@smartling.com>
2024-04-17 15:52:36 +0200

    test (WP-881)

--- inc/Smartling/ContentTypes/Elementor/Elements/Tabs.php
+++ inc/Smartling/ContentTypes/Elementor/Elements/Tabs.php
@@ -2,6 +2,10 @@
 
 namespace Smartling\ContentTypes\Elementor\Elements;
 
+use Smartling\Models\RelatedContentInfo;
+use Smartling\Submissions\SubmissionEntity;
+use Smartling\Submissions\SubmissionManager;
+
 class Tabs extends Unknown {
     public function getType(): string
     {
@@ -22,5 +26,25 @@ class Tabs extends Unknown {
         }
 
         return [$this->getId() => $return];
+    }
+
+    public function setTargetContent(RelatedContentInfo $info, array $strings, SubmissionEntity $submission, SubmissionManager $submissionManager,): static
+    {
+        foreach ($strings[$this->id] ?? [] as $path => $array) {
+            if (is_array($array)) {
+                foreach ($array as $id => $values) {
+                    foreach ($this->settings['tabs'] ?? [] as $index => $tab) {
+                        if ($tab['_id'] ?? '' === $id) {
+                            foreach ($values as $property => $value) {
+                                $this->settings['tabs'][$index][$property] = $value;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        $this->raw['settings'] = $this->settings;
+
+        return new static($this->raw);
     }
 }
