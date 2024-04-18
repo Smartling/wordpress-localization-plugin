552847fe2ba96317ea6e1d01930c32183995d887
Vitalii Solovei <vsolovei@smartling.com>
2024-04-18 20:40:03 +0200

    test (WP-881)

--- inc/Smartling/ContentTypes/Elementor/ElementAbstract.php
+++ inc/Smartling/ContentTypes/Elementor/ElementAbstract.php
@@ -196,9 +196,11 @@ abstract class ElementAbstract implements Element {
         }
         try {
             $tagData['settings'][self::SETTING_KEY_POPUP] = $value; // $value must be string in order to be converted back to tag
-            $this->getLogger()->debug(json_encode($tagData));
-            usleep(1000);
-            return $dynamicTagsManager->tag_data_to_tag_text(...array_values($tagData));
+            $result = $dynamicTagsManager->tag_data_to_tag_text(...array_values($tagData));
+            usleep(10000);
+            $this->getLogger()->debug("Text: $result");
+
+            return $result;
         } catch (\Throwable $e) {
             $this->getLogger()->warning("Unable to convert Elementor tag data to text: {$e->getMessage()}");
         }
