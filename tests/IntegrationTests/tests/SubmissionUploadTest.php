18552093b3314b84b361e254a666a8690fa36d15
Vitalii Solovei <vsolovei@smartling.com>
2024-05-30 14:42:04 +0200

    improve test (WP-889)

--- tests/IntegrationTests/tests/SubmissionUploadTest.php
+++ tests/IntegrationTests/tests/SubmissionUploadTest.php
@@ -2,6 +2,9 @@
 
 namespace IntegrationTests\tests;
 
+use Smartling\ApiWrapperInterface;
+use Smartling\ContentTypes\ContentTypeHelper;
+use Smartling\DbAl\SmartlingToCMSDatabaseAccessWrapperInterface;
 use Smartling\Models\JobInformation;
 use Smartling\Models\UserTranslationRequest;
 use Smartling\Services\ContentRelationsDiscoveryService;
@@ -12,19 +15,30 @@ class SubmissionUploadTest extends SmartlingUnitTestCaseAbstract
 {
     public function testUploadMultipleTargets()
     {
+        $api = $this->getContainer()->get('api.wrapper.with.retries');
+        assert($api instanceof ApiWrapperInterface);
+        $jobName = uniqid('test multiple uploads job ', true);
+        $locales = [];
         $postId = $this->createPost();
+        $profile = $this->getSettingsManager()->getSingleSettingsProfile(1);
         $submissionManager = $this->getSubmissionManager();
-        $this->assertEquals(0, $submissionManager->find([SubmissionEntity::FIELD_SOURCE_ID => $postId]), "Expected no submissions to exist for postId=$postId");
+        $targetBlogs = [2, 3];
+        foreach ($profile->getTargetLocales() as $locale) {
+            if (in_array($locale->getBlogId(), $targetBlogs)) {
+                $locales[] = $locale->getSmartlingLocale();
+            }
+        }
+        $job = $api->createJob($profile, ['name' => $jobName, 'locales' => $locales]);
+        $this->assertCount(0, $submissionManager->find([SubmissionEntity::FIELD_SOURCE_ID => $postId]), "Expected no submissions to exist for postId=$postId");
         $service = $this->getContainer()->get('service.relations-discovery');
         assert($service instanceof ContentRelationsDiscoveryService);
-        $service->createSubmissions(new UserTranslationRequest($postId, 'post', [], [2, 3], new JobInformation(
-            'test',
-            false,
-            'Test multiple targets',
-            '',
-            '',
-            '',
-        )));
+        $service->createSubmissions(new UserTranslationRequest(
+            $postId,
+            ContentTypeHelper::CONTENT_TYPE_POST,
+            [],
+            $targetBlogs,
+            new JobInformation($job['translationJobUid'], false, $jobName, '', '', ''),
+        ));
         $submissions = $submissionManager->find([SubmissionEntity::FIELD_SOURCE_ID => $postId]);
         $this->assertCount(2, $submissions, 'Expected two new submissions to be created');
         $submissionIds = array_reduce($submissions, static function (SubmissionEntity $entity) {
@@ -35,6 +49,9 @@ class SubmissionUploadTest extends SmartlingUnitTestCaseAbstract
         $submissions = $submissionManager->find([SubmissionEntity::FIELD_SOURCE_ID => $postId]);
         foreach ($submissions as $submission) {
             $this->assertNotEquals(0, $submission->getTargetId());
+            $this->forceSubmissionDownload($submission);
+            $submission = $submissionManager->getEntityById($submission->getId());
+            $this->assertNotEquals(SubmissionEntity::SUBMISSION_STATUS_FAILED, $submission->getStatus());
         }
     }
 }
